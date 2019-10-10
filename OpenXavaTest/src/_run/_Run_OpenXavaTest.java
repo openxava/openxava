0db20c982d227b7d8aa843d1c8b38781e97d4851
javierpaniza <4420198+javierpaniza@users.noreply.github.com>
2019-10-10 12:42:08 +0200

    Fix: Wrong visual style for subcontrollers in collections

--- OpenXavaTest/src/_run/_Run_OpenXavaTest.java
+++ OpenXavaTest/src/_run/_Run_OpenXavaTest.java
@@ -1,18 +1,18 @@
-package _run; 
+package _run;
 
 import org.openxava.util.*;
 
 /**
  * Execute this class to start the application.
- * 
+ *
  * With Eclipse: Right mouse button > Run As > Java Application
  */
 
-public class _Run_OpenXavaTest { 
+public class _Run_OpenXavaTest {
 
 	public static void main(String[] args) throws Exception {
 		DBServer.start("OpenXavaTestDB"); // To use your own database comment this line and configure web/META-INF/context.xml
-        AppServer.run("OpenXavaTest");
+		AppServer.run("OpenXavaTest");
 	}
 
 }
