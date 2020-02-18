e2dd796bb172f21cf97e3c17803eb39f30db2ad1
javierpaniza <4420198+javierpaniza@users.noreply.github.com>
2020-02-18 14:16:33 +0100

    Suite executed. Icon color adjusted. Working on doc.

--- OpenXava/src/org/openxava/formatters/EnumListFormatter.java
+++ OpenXava/src/org/openxava/formatters/EnumListFormatter.java
@@ -1,11 +1,9 @@
 package org.openxava.formatters;
 
 import javax.servlet.http.*;
-
-import org.apache.commons.beanutils.*;
 import org.openxava.model.*;
 import org.openxava.model.meta.*;
-import org.openxava.util.*;
+import org.openxava.web.style.*;
 
 /**
  * tmp
@@ -15,29 +13,18 @@ public class EnumListFormatter implements IMetaPropertyFormatter {
 
 
 	public String format(HttpServletRequest request, MetaProperty metaProperty, Object object) throws Exception {
-		System.out.println("[EnumListFormatter.parse] string.3=" + object); // tmp
-		/*
-		// tmp return object==null?"":object.toString();
-		try {
-			String icon = BeanUtils.getProperty(object, "icon");
-			if (!Is.emptyString(icon)) {
-				return "<i class='mdi mdi-" + icon + "'></i> " + metaProperty.format(object, Locales.getCurrent());
-			}
-		}
-		catch (Exception ex) {
-			ex.printStackTrace(); // tmp Quitar
-		}
-		*/
-		// tmp ini
-		String label = metaProperty.format(object, Locales.getCurrent());
+		// tmp String label = metaProperty.format(object, Locales.getCurrent());
+		if (object == null) return "";
+		String label = object instanceof Number?
+			metaProperty.getValidValueLabel(((Number) object).intValue()):
+			metaProperty.getValidValueLabel(object);
+		Style style = (Style) request.getAttribute("style");	
 		if (object instanceof IIconEnum) {
-			return "<i class='mdi mdi-" + ((IIconEnum) object).getIcon() + 
+			return "<span class='" + style.getIconInList() + "'><i class='mdi mdi-" + ((IIconEnum) object).getIcon() + 
 				"' title='" + label + "'></i>" +
-				"<span style='display: none'>" + label + "</span>";
+				"<span style='display:none'>" + label + "</span></span>";
 		}
 		return label;
-		// tmp fin
-		// tmp return metaProperty.format(object, Locales.getCurrent());
 	}
 
 	public Object parse(HttpServletRequest request, MetaProperty metaProperty, String string) throws Exception {
