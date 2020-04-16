6b52b12ea2d3e1fabc626380fab7c0083f778372
afernandez <afernandez@gestion400.com>
2020-04-16 07:51:36 +0200

    fix: bigDecimalValidator with a BigDecimal with 7 or more decimals

--- OpenXavaTest/pojo-src/org/openxava/test/tests/BigDecimalValidatorTest.java
+++ OpenXavaTest/pojo-src/org/openxava/test/tests/BigDecimalValidatorTest.java
@@ -5,25 +5,22 @@ import java.math.*;
 import org.openxava.util.*;
 import org.openxava.validators.*;
 
+import junit.framework.TestCase;
+
 /**
  * @author Ana Andres
  * Created on 15 abr. 2020
  */
-public class BigDecimalValidatorTest {
+public class BigDecimalValidatorTest extends TestCase {
 
-	public static void main(String...strings ) {
-		try {
-			BigDecimalValidator validator = new BigDecimalValidator();
-			Messages errors = new Messages();
-			BigDecimal value = new BigDecimal("0.0000000");
-			validator.setMaximumFractionDigits(7);
-			validator.setMaximumIntegerDigits(2);
-			validator.validate(errors, value, "", "");
-			System.out.println("[BigDecimalValidatorTest.main] errores:"+ errors.toString()); // tmp
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void testSevenDecimals() throws Exception {
+		BigDecimalValidator validator = new BigDecimalValidator();
+		Messages errors = new Messages();
+		BigDecimal value = new BigDecimal("0.0000000");
+		validator.setMaximumFractionDigits(7);
+		validator.setMaximumIntegerDigits(2);
+		validator.validate(errors, value, "", "");
+		assertTrue(Is.empty(errors.toString()));
 	}
 	
 }
