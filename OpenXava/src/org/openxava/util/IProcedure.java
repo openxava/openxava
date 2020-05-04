94566e3ef643dcb16465c316bccc05b8faf713e5
Javier Paniza <javier@192.168.0.193>
2020-05-04 11:13:29 +0200

    New IProcedure functional interface that does not receive or return any value to be use in lambdas

--- OpenXava/src/org/openxava/util/IProcedure.java
+++ OpenXava/src/org/openxava/util/IProcedure.java
@@ -3,7 +3,7 @@ package org.openxava.util;
 /**
  * Functional interface that does not receive or return any value. <p>
  * 
- * Intended to use in lambdas.
+ * Intended to be used in lambdas.
  * 
  * @since 6.3.2
  * @author Javier Paniza
