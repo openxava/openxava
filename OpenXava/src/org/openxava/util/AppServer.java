d0d9b323d49bd8530d720e2cd54c04f2220d6848
Javier Paniza <javier@192.168.0.193>
2020-09-04 19:27:48 +0200

    - Fix: When running from application launcher the default i18n does not work - Fix: The updateOX Ant target fails after a clean of OpenXava project. - Fix: Not applicable warnings on updateOX and deployWar Ant targets.

--- OpenXava/src/org/openxava/util/AppServer.java
+++ OpenXava/src/org/openxava/util/AppServer.java
@@ -2,6 +2,11 @@ package org.openxava.util;
 
 import java.io.*;
 import org.apache.catalina.startup.*;
+import org.apache.commons.logging.*;
+
+import java.nio.file.*;
+import java.nio.file.Files;
+import java.util.*;
 
 /**
  * 
@@ -9,8 +14,12 @@ import org.apache.catalina.startup.*;
  */
 public class AppServer { 
 	
+	private static final String I18N_DIR = "web/WEB-INF/classes/"; 
+	private final static Log log = LogFactory.getLog(AppServer.class);
+	
 	public static void run(String app) throws Exception {
-		System.out.println(XavaResources.getString("starting_application")); 
+		System.out.println(XavaResources.getString("starting_application"));
+		createDefaultI18nFiles(app); 
         String webappDir = new File("web").getAbsolutePath();
         Tomcat tomcat = new Tomcat();
         tomcat.setBaseDir("temp"); 
@@ -23,6 +32,38 @@ public class AppServer {
         System.out.println(XavaResources.getString("application_started_go", "http://localhost:8080/" + app));  
         
         tomcat.getServer().await();
+	}
+	
+	private static void createDefaultI18nFiles(String app) {
+		try { 
+			Path labelsEsPath = Paths.get(I18N_DIR + "Etiquetas" + app + "_es.properties");
+			if (Files.exists(labelsEsPath)) {
+				Files.copy(labelsEsPath, java.nio.file.Paths.get(I18N_DIR + "Etiquetas" + app + ".properties"), StandardCopyOption.REPLACE_EXISTING);
+				Files.copy(Paths.get(I18N_DIR + "Mensajes" + app + "_es.properties"), Paths.get(I18N_DIR + "Mensajes" + app + ".properties"), StandardCopyOption.REPLACE_EXISTING);
+			}
+			else {
+				if (copyDefaultI18nFile(app, "en")) return; 
+				if (copyDefaultI18nFile(app, Locale.getDefault().getLanguage())) return;				
+				String [] popularLanguages = { "de", "fr", "it", "es", "ru", "zh", "ja", "in", "pl", "sr", "sv", "ca" };
+				for (String language: popularLanguages) {
+					if (copyDefaultI18nFile(app, language)) return;
+				}
+				for (String language: Locale.getISOLanguages()) {
+					if (copyDefaultI18nFile(app, language)) return;
+				}				
+			}
+		}
+		catch (Exception ex) {
+			log.warn(XavaResources.getString("default_i18n_files_not_created"), ex);
+		}
+	}
+	
+	private static boolean copyDefaultI18nFile(String app, String language) throws Exception {
+		Path labelsPath = Paths.get(I18N_DIR + app + "-labels_" + language + ".properties");
+		if (!Files.exists(labelsPath)) return false; 
+		Files.copy(labelsPath, Paths.get(I18N_DIR + app + "-labels.properties"), StandardCopyOption.REPLACE_EXISTING);
+		Files.copy(Paths.get(I18N_DIR + app + "-messages_" + language + ".properties"), Paths.get(I18N_DIR + app + "-messages.properties"), StandardCopyOption.REPLACE_EXISTING);		
+		return true;
 	}
 
 }
