da79112ae4bb5175b69520f645b0fd1152f98f0e
Javier Paniza <javier@192.168.0.193>
2020-12-01 22:14:43 +0100

    Ant target runDBManager in OpenXava/build.xml

---
+++ OpenXava/src/org/openxava/tools/DBManager.java
@@ -0,0 +1,20 @@
+package org.openxava.tools;
+
+import org.openxava.util.*;
+
+/**
+ * Execute this class to start a manager for you development database.
+ * 
+ * With Eclipse: Right mouse button > Run As > Java Application
+ * 
+ * @since 6.5
+ * @author javie Javier Paniza
+ */
+
+public class DBManager {
+	
+	public static void main(String[] args) {
+		DBServer.runManager();
+	}	
+
+}
