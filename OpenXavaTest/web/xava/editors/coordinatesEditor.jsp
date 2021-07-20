f914e697d27f538bce5709f81a8e03b7783b0d63
Javier Paniza <javier@192.168.0.193>
2021-07-20 15:08:27 +0200

    When coordinates change the map is updated. If the record has already coordinates the map is shown correctly.

--- OpenXavaTest/web/xava/editors/coordinatesEditor.jsp
+++ OpenXavaTest/web/xava/editors/coordinatesEditor.jsp
@@ -2,7 +2,7 @@
 
 <div class="ox-coordinates-map"> <%-- tmp Mover a Style --%>
 	
-	<div id="mapid" style="height: 450px; width: 600px"> <%-- tmp Ha de ser una clase, no un id --%>
+	<div class="xava_coordinates" style="height: 450px; width: 600px"> <%-- tmp Ha de ser una clase, no un id --%>
 	
 	</div>
 	
--- OpenXavaTest/web/xava/editors/js/coordinatesEditor.js
+++ OpenXavaTest/web/xava/editors/js/coordinatesEditor.js
@@ -1,13 +1,27 @@
+if (coordinatesEditor == null) var coordinatesEditor = {};
+
 openxava.addEditorInitFunction(function() {
-	var mymap = L.map('mapid').setView([39.45399631909972, -0.3838714157032522], 16);
-	// TMP PONER LA ATRIBUTION BIEN
-	// TMP ME QUEDÉ POR AQUÍ: FALLA DICIENDO "Map container not found"
-	/*
-	L.tileLayer('https://b.tile.opentopomap.org/{z}/{x}/{y}.png', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-			'Imagery &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-		id: 'mapbox/streets-v11'
-	}).addTo(mymap);
-	*/
+	$(".xava_coordinates").each(function() {
+		// var map = L.map(this).setView([39.45399631909972, -0.3838714157032522], 16);
+		var map = L.map(this); // tmp .setView([39.45399631909972, -0.3838714157032522], 16);
+		L.tileLayer('https://b.tile.opentopomap.org/{z}/{x}/{y}.png', {
+			maxZoom: 18,
+			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+				'Imagery &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+		}).addTo(map);
+		
+		var input = $(this).parent().parent().find("input[type='text']");
+		coordinatesEditor.setView(map, input);		
+		 	
+		input.change(function() {
+			coordinatesEditor.setView(map, input);
+		});
+	});
 });
+
+coordinatesEditor.setView = function(map, input) {
+	var coordinates = input.val().split(/,/);
+	// TMP ME QUEDÉ POR AQUÍ: FALLA CUANDO EL CAMPO ESTÁ VACÍO
+	console.log("[coordinatesEditor.setView] coordinates=" + coordinates)
+	map.setView(coordinates, 16);
+}
