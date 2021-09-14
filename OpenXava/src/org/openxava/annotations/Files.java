dc1c0bdd5a7a77d8f7c7b664019ada91176fc800
Javier Paniza <javier@192.168.0.193>
2021-09-14 17:05:16 +0200

    @Files annotation created. Working in example, in Movie3

---
+++ OpenXava/src/org/openxava/annotations/Files.java
@@ -0,0 +1,27 @@
+package org.openxava.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * tmp Redoc
+ * Associates an action to a property or reference in the view. <p>
+ * 
+ * Applies to properties and references.<p>
+ * 
+ * Example:
+ * <pre>
+ * &nbsp;@Action("Deliveries.generateNumber")
+ * &nbsp;private int number;
+ * </pre>
+ * The actions are displayed as a link or an image beside the property.<br>
+ * By default the action link is present only when the property is editable, 
+ * but if the property is read only or calculated then it is always present.<br>
+ * 
+ * @author Javier Paniza
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.METHOD })
+public @interface Files {
+	
+}
---
+++ OpenXavaTest/pojo-src/org/openxava/test/model/Movie3.java
@@ -0,0 +1,127 @@
+package org.openxava.test.model;
+
+import java.util.*;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+import org.openxava.jpa.*;
+import org.openxava.model.*;
+
+/**
+ * tmp
+ * Model to test concatenated reports using 
+ * {@link org.openxava.test.actions.MovieReportAction MovieReportAction} and to 
+ * test stereotypes FILE and FILES. <p>
+ * 
+ * @author Jeromy Altuna
+ */
+@Entity
+@Table(name="Movie")
+@View(members=
+	"data sheet ["   +
+	"   title, releaseDate;" +	
+	"   director;"   +
+	"   writers;"    +
+	"   starring;"   +
+	"] " +
+	"Multimedia 1 {" +
+	"   trailer;"    +
+	"   scripts;" +
+	"}" +
+	"Multimedia 2 {" +
+	"   photographs" +
+	"}"
+)
+@Tab(properties="title, director, writers, releaseDate, trailer")
+public class Movie3 extends Identifiable {
+
+	private String title;
+	private String director;
+	private String writers;
+	private String starring;
+	
+	private Date releaseDate;
+	
+	@Files
+	@Column(length=32)
+	private String trailer;
+	
+	@Files
+	@Column(length=32)	
+	private String scripts;
+	
+	@Files
+	@Column(length=32)
+	private String photographs;
+	
+	public static Movie3 findById(String id) {
+		Query query = XPersistence.getManager().createQuery("from Movie m where m.id = :id");
+		query.setParameter("id", id);
+		return (Movie3) query.getSingleResult();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getWriters() {
+		return writers;
+	}
+
+	public void setWriters(String writers) {
+		this.writers = writers;
+	}
+
+	public String getStarring() {
+		return starring;
+	}
+
+	public void setStarring(String starring) {
+		this.starring = starring;
+	}
+
+	public String getTrailer() {
+		return trailer;
+	}
+
+	public void setTrailer(String trailer) {
+		this.trailer = trailer;
+	}
+
+	public String getScripts() {
+		return scripts;
+	}
+
+	public void setScripts(String scripts) {
+		this.scripts = scripts;
+	}
+
+	public String getPhotographs() {
+		return photographs;
+	}
+
+	public void setPhotographs(String photographs) {
+		this.photographs = photographs;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}	
+}
