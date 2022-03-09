c9f42baf448458a155d22a7b2c6539b770e0fa98
javierpaniza <javierpaniza>
2022-03-09 13:39:20 +0100

    Creating a case to reproduce the bug

--- OpenXavaTest/pojo-src/org/openxava/test/model/InvoicesByYear.java
+++ OpenXavaTest/pojo-src/org/openxava/test/model/InvoicesByYear.java
@@ -1,5 +1,6 @@
 package org.openxava.test.model;
 
+import java.math.*;
 import java.util.*;
 
 import javax.validation.constraints.*;
@@ -21,6 +22,13 @@ public class InvoicesByYear {
 	int year;
 	
 	@Condition("${year} = ${this.year}")
+	@ListProperties("year, number, date, total[invoicesByYear.totalSum]") // tmr
 	Collection<Invoice> invoices;
+	
+	public BigDecimal getTotalSum() {
+		// TMR ME QUEDÉ POR AQUÍ, INTENTANDO REPRODUCIR, ANTES DE HACER LA PRUEBA JUNIT
+		// tmr .reduce(BigDecimal.ZERO, BigDecimal::add);
+		return new BigDecimal("100");
+	}
 
 }
