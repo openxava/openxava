d64e7e54496866a7e84eb6ff1d7834ee46a61ea9
javierpaniza <4420198+javierpaniza@users.noreply.github.com>
2022-06-01 15:01:28 +0200

    Spanish archetype almost finished. Testing it, problem with enconding.

--- openxava-archetype/src/main/java/com/mycompany/myapp/run/myapp.java
+++ openxava-archetype/src/main/java/com/mycompany/myapp/run/myapp.java
@@ -5,13 +5,13 @@ import org.openxava.util.*;
 /**
  * Execute this class to start the application.
  *
- * With Eclipse: Right mouse button > Run As > Java Application
+ * With OpenXava Studio/Eclipse: Right mouse button > Run As > Java Application
  */
 
 public class myapp {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("myapp-db"); // To use your own database comment this line and configure web/META-INF/context.xml
+		DBServer.start("myapp-db"); // To use your own database comment this line and configure src/main/webapp/META-INF/context.xml
 		AppServer.run("myapp"); // Use AppServer.run("") to run in root context
 	}
 
