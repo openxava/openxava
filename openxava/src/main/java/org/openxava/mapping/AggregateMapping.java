39e8385ab7cbb0d3391f82ca77564645ecff61a1
javierpaniza <javierpaniza>
2022-10-20 20:00:52 +0200

    References in embeddable no longer added to MetaModel from mapping

--- openxava/src/main/java/org/openxava/mapping/AggregateMapping.java
+++ openxava/src/main/java/org/openxava/mapping/AggregateMapping.java
@@ -12,20 +12,7 @@ import org.openxava.util.*;
 public class AggregateMapping extends ModelMapping {
 
 	private String modelName;
-	
-	/* tmr	
-	public void addReferenceMapping(ReferenceMapping referenceMapping) throws XavaException { 		
-		super.addReferenceMapping(referenceMapping);
-		if (!getMetaModel().containsMetaReference(referenceMapping.getReference())) {
-			MetaReference r = new MetaReference();
-			r.setReferencedModelName(Strings.firstUpper(referenceMapping.getReference()));
-			r.setAggregate(false); 
-			getMetaModel().addMetaReference(r);
-		}
-
-	}
-	*/
-	
+		
 	public String getModelName() {
 		return modelName;
 	}
