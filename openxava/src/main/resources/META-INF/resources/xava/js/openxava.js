8b2c5e60b9bb6b1b507e1dec31d79dc35c206c3a
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-11-22 14:07:20 -0300

    v1.33

--- openxava/src/main/resources/META-INF/resources/xava/js/openxava.js
+++ openxava/src/main/resources/META-INF/resources/xava/js/openxava.js
@@ -260,7 +260,20 @@ openxava.initPlaceholder = function(){
 }
 
 openxava.initTooltips = function() { 
-	$(".xava_editor").find("input", ".xava_select", ".editor").tooltip({ 
+	$(".xava_editor", ".editor").find("input").tooltip({ 
+		position: {
+			my: "left+3 center", at: "right center", 
+	        using: function( position, feedback ) {
+			$( this ).css( position );
+			$( "<div>" )
+				.addClass( "arrow" )
+				.addClass( feedback.vertical )
+				.addClass( feedback.horizontal )
+				.appendTo( this );
+	        }
+		}
+	});	
+	$(".editor").find("input").tooltip({ 
 		position: {
 			my: "left+3 center", at: "right center", 
 	        using: function( position, feedback ) {
