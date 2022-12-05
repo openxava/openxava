7c3b80f01e39a5d1127004a34f9b94739486f580
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-12-05 15:28:10 -0300

    change test name

--- openxavatest/src/test/java/org/openxava/test/tests/CustomerRadioButtonTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/CustomerRadioButtonTest.java
@@ -27,7 +27,7 @@ public class CustomerRadioButtonTest extends ModuleTestBase {
 		assertValue("type", "2");	// special
 	}
 	
-	public void testEditorByView_radioButton() throws Exception { 
+	public void testEditorByView_radioButton_radioButtonNoEditable() throws Exception { 
 		// Really editor by property-view must be tested visually
 		// and about radioButton we only test that it's possible to use in junit test,
 		// because behaves equals that a combo. Hence it's needed to test visually
