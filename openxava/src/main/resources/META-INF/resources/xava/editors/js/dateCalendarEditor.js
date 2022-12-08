288b0837ca9f088bc1534e29d8ec2c3a9a56e250
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-12-08 16:22:21 -0300

    fixed

--- openxava/src/main/resources/META-INF/resources/xava/editors/js/dateCalendarEditor.js
+++ openxava/src/main/resources/META-INF/resources/xava/editors/js/dateCalendarEditor.js
@@ -24,42 +24,43 @@ openxava.addEditorInitFunction(function() {
         var date = withEnter?enterDate:$(this).val();
         if (date === "") return;
         date = date.trim();
-        var separator = dateFormat.substr(1, 1);
-        var idx = date.lastIndexOf(separator);
-        if (idx < 0) {
-            if (date.length % 2 != 0) date = " " + date;
-            var inc = dateFormat.substr(0, 1) === 'Y'?2:0;
-            var last = date.substring(4 + inc);
-            var middle = date.substring(2 + inc, 4 + inc);
-            var first = date.substring(0, 2 + inc);
-            date = first + separator + middle + separator + last;
-            date = date.trim();
+        if (date.length > 5 && !date.includes(":")) {
+            var separator = dateFormat.substr(1, 1);
+            var idx = date.lastIndexOf(separator);
+            if (idx < 0) {
+                if (date.length % 2 != 0) date = " " + date;
+                var inc = dateFormat.substr(0, 1) === 'Y'?2:0;
+                var last = date.substring(4 + inc);
+                var middle = date.substring(2 + inc, 4 + inc);
+                var first = date.substring(0, 2 + inc);
+                date = first + separator + middle + separator + last;
+                date = date.trim();
+            }
+            idx = date.lastIndexOf(separator);
+            var idxSpace = date.indexOf(' ');
+            var pureDate = date;
+            var time = "";
+            if (idxSpace >= 0) {
+                time = date.substr(idxSpace);
+                pureDate = date.substr(0, idxSpace);
+            }
+            var suffix = "";
+            if (idx == pureDate.length - 1) {
+                pureDate = pureDate.substring(0, idx);
+                suffix = separator;
+                idx = pureDate.lastIndexOf(separator);
+            }
+            if (dateFormat.indexOf('Y') > 0 && pureDate.length - idx < 4) {
+                var dateNoYear = pureDate.substring(0, idx);
+                var year = pureDate.substring(idx + 1);
+                var prefix = year > 50?"19":"20";
+                date = dateNoYear + separator + prefix + year + suffix + time;
+            }
+            date = date.includes(".20 ") ? date.replace(".20 ", " ") : date;
+            $(this).val(date);
+            enterDate = undefined;
+            withEnter = false;
         }
-        idx = date.lastIndexOf(separator);
-        var idxSpace = date.indexOf(' ');
-        var pureDate = date;
-        var time = "";
-        if (idxSpace >= 0) {
-            time = date.substr(idxSpace);
-            pureDate = date.substr(0, idxSpace);
-        }
-        var suffix = "";
-        if (idx == pureDate.length - 1) {
-            pureDate = pureDate.substring(0, idx);
-            suffix = separator;
-            idx = pureDate.lastIndexOf(separator);
-        }
-        if (dateFormat.indexOf('Y') > 0 && pureDate.length - idx < 4) {
-            var dateNoYear = pureDate.substring(0, idx);
-            var year = pureDate.substring(idx + 1);
-            var prefix = year > 50?"19":"20";
-            date = dateNoYear + separator + prefix + year + suffix + time;
-        }
-        date = date.includes(".20 ") ? date.replace(".20 ", " ") : date;
-        $(this).first().val(date);
-        //$(this).val(date);
-        enterDate = undefined;
-        withEnter = false;
     });
     $('.flatpickr-calendar').remove();
     $('.xava_date').flatpickr({
