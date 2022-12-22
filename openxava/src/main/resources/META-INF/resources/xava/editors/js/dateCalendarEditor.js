94d1e37957c23968d7cab4b2c94f03036f3869ca
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2022-12-22 15:13:40 -0300

    testing calendar behavior

--- openxava/src/main/resources/META-INF/resources/xava/editors/js/dateCalendarEditor.js
+++ openxava/src/main/resources/META-INF/resources/xava/editors/js/dateCalendarEditor.js
@@ -6,17 +6,28 @@ openxava.addEditorInitFunction(function() {
     var withEnter = false;
     var enterDate;
     var onOpenDateTime;
-
+    var validDate = false;
+    var regExp = /[0-9]+:\.\/-/g;
+    
     $('.xava_date > input').keydown(function(event) {
+        console.log("keydown");
         var keycode = event.keyCode || event.which;
         if (keycode == 13) {
             enterDate = $(this).val();
+            validDate = regExp.test(enterDate)?true:false;
             if ((enterDate.includes("/") || enterDate.includes(".") || enterDate.includes("-")) && enterDate.length > 9) {
                 withEnter = false;
             } else {
+                console.log("enter");
                 withEnter = true;
             }
         }
+    });
+    
+    $('.xava_date > input').on('blur', function() {
+        enterDate = $(this).val();
+        validDate = regExp.test(enterDate)?true:false;
+        console.log(validDate);
     });
     
     $('.xava_date > input').change(function() {
@@ -31,10 +42,15 @@ openxava.addEditorInitFunction(function() {
             if (idx < 0) {
                 if (date.length % 2 != 0) date = " " + date;
                 var inc = dateFormat.substr(0, 1) === 'Y'?2:0;
+                console.log(inc);
                 var last = date.substring(4 + inc);
+                console.log(last);
                 var middle = date.substring(2 + inc, 4 + inc);
+                console.log(middle);
                 var first = date.substring(0, 2 + inc);
+                console.log(first);
                 date = first + separator + middle + separator + last;
+                console.log(date);
                 date = date.trim();
             }
             idx = date.lastIndexOf(separator);
@@ -59,6 +75,8 @@ openxava.addEditorInitFunction(function() {
             }
             date = date.includes(".20 ") ? date.replace(".20 ", " ") : date;
             $(this).val(date);
+            console.log("function");
+            console.log(date);
             enterDate = undefined;
             withEnter = false;
         }
@@ -70,9 +88,17 @@ openxava.addEditorInitFunction(function() {
         wrap: true,
         locale: openxava.language,
         onOpen: function(selectedDates, dateStr, instance) {
+            console.log("onOpen");
+            console.log(selectedDates);
+            console.log(dateStr);
             onOpenDateTime = dateStr;
         },
         onChange: function(selectedDates, dateStr, instance) {
+            dateStr = validDate?dateStr:$('.xava_date > input').val('');
+            selectedDates  = validDate?selectedDates:$('.xava_date > input').val('');
+            console.log("onChange");
+            console.log(selectedDates);
+            console.log(dateStr);
             if (onOpenDateTime != null) {
                 if (onOpenDateTime.length > 10) {
                     $(instance.input).data("changedCancelled", true);
@@ -87,11 +113,16 @@ openxava.addEditorInitFunction(function() {
                 if (dateStr === $(instance.input).attr('value')) {
                     $(instance.input).data("changedCancelled", true);
                 } else {
+                    console.log("else");
+                    console.log(dateStr);
                     $(instance.input).attr('value', dateStr);
                 }
             }
         },
         onClose: function(selectedDates, dateStr, instance) {
+            console.log("onClose");
+            console.log(selectedDates);
+            console.log(dateStr);
             if (onOpenDateTime != null) {
                 if (onOpenDateTime == dateStr) {
                     $(instance.input).data("changedCancelled", true);
