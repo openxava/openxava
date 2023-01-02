74748bec1cc8b39edc517a52a6d0c70be78cb021
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2023-01-02 10:29:57 -0300

    added explanation

--- openxava/src/main/java/org/openxava/annotations/ZerosFilled.java
+++ openxava/src/main/java/org/openxava/annotations/ZerosFilled.java
@@ -2,6 +2,23 @@ package org.openxava.annotations;
 
 import java.lang.annotation.*;
 
+/**
+ * Used to fill the field with zeros, the amount of zero is what remains to complete the field. (Leading zero) <p>	
+ *
+ * Applies to properties.<p>
+ * 
+ * Example:
+ * <pre>
+ * &nbsp;@ZerosFilled
+ * &nbsp;@Column(length = 4)
+ * &nbsp;private int number;
+ * </pre>
+ * 
+ * It's synonymous of @Stereotype("ZEROS_FILLED") and @Stereotype("RELLENADO_CON_CEROS")
+ * 
+ * @author Chungyen Tsai
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.FIELD, ElementType.METHOD })
 public @interface ZerosFilled {
