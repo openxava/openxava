c89e2ae87811f3cb245a6618de4c059504f3101c
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2023-01-16 07:54:56 -0300

    fix messages quote issue and catalan @File/Files error dialog

--- openxava/src/main/java/org/openxava/web/taglib/MessageTag.java
+++ openxava/src/main/java/org/openxava/web/taglib/MessageTag.java
@@ -48,7 +48,6 @@ public class MessageTag extends TagSupport {
 			else {
 				string = XavaResources.getString(getKey()); 
 			}
-			string = string.replace("'", "\'+ \"'\" +\'");
 			pageContext.getOut().print(string);
 		}
 		catch (Exception ex) {
