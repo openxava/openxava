2287f793b3947d7a2411ad501a1df73fc6cf2b6d
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2023-02-15 14:35:11 -0300

    a

--- openxava/src/main/java/org/openxava/web/servlets/CSSServlet.java
+++ openxava/src/main/java/org/openxava/web/servlets/CSSServlet.java
@@ -25,7 +25,14 @@ public class CSSServlet extends HttpServlet {
 		try {
 			if (request.getRequestURI().endsWith(".css")) {
 				String path = request.getServletPath() + request.getPathInfo();
-				InputStream inputStream = getClass().getClassLoader().getResourceAsStream("META-INF/resources/" + path);
+				System.out.println(request.getServletPath());
+				System.out.println(request.getPathInfo());
+				//InputStream inputStream = getClass().getClassLoader().getResourceAsStream("META-INF/resources/" + path);
+				InputStream inputStream = Resources.getAsStreamInPrefixes(request.getPathInfo().toString(), request.getServletPath().toString());
+				//URL url = Resources.getAsStreamInPrefixes(path, "META-INF/resources/", "../webapp/", "/");
+				
+				
+				System.out.println(inputStream.getClass().getResource(path).getPath());
 				StringWriter writer = new StringWriter();
 				IOUtils.copy(inputStream, writer, StandardCharsets.UTF_8);
 				String data = writer.toString().replaceAll("@import (['\"].*)\\.css", "@import $1.css?ox=" + ModuleManager.getVersion());
