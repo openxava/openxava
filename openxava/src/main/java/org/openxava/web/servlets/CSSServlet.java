39dcd872e374a1182b59237c5261322609876d88
javierpaniza <javierpaniza>
2023-02-16 21:27:53 +0100

    Testing and refining code

--- openxava/src/main/java/org/openxava/web/servlets/CSSServlet.java
+++ openxava/src/main/java/org/openxava/web/servlets/CSSServlet.java
@@ -23,6 +23,7 @@ public class CSSServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
+			long ini = System.currentTimeMillis(); // tmr
 			if (request.getRequestURI().endsWith(".css")) {
 				InputStream inputStream = getCSSAsStream(request.getPathInfo(), request.getServletPath());
 				StringWriter writer = new StringWriter();
@@ -31,19 +32,34 @@ public class CSSServlet extends HttpServlet {
 				
 				response.getWriter().append(data);
 			}
+			long cuesta = System.currentTimeMillis() - ini; // tmr
+			System.out.println("[CSSServlet.doGet] " + request.getRequestURI() + "=" + cuesta); // tmp
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 			throw new ServletException(XavaResources.getString("attachments_css_servlet_error", request.getServletPath() + request.getPathInfo()));
 		}
 	}
 	
-	private InputStream getCSSAsStream(String resourceName, String prefix) { 
+	private InputStream getCSSAsStream(String resourceName, String prefix) {
+		// TMR ME QUEDÉ POR AQUÍ
+		// TMR NO HACE CACHÉ
+		/* TMR NOTAS:
+		 * - rose no pink
+		   - } catch (Exception e) { } : No
+		   - materialdesignicons.css.map No se devolvía
+		 * 
+		 */
 		InputStream stream = null;
 		try {
-			if (getClass().getClassLoader().getResourceAsStream("META-INF/resources/" + prefix + resourceName) != null) return stream = getClass().getClassLoader().getResourceAsStream("META-INF/resources/" + prefix + resourceName);
+			// tmr if (getClass().getClassLoader().getResourceAsStream("META-INF/resources/" + prefix + resourceName) != null) return stream = getClass().getClassLoader().getResourceAsStream("META-INF/resources/" + prefix + resourceName);
+			// tmr ini
+			stream = getClass().getClassLoader().getResourceAsStream("META-INF/resources/" + prefix + resourceName);
+			if (stream != null) return stream;
+			// tmr fin
 			
 			FileInputStream file = new FileInputStream(getServletContext().getRealPath("/") + prefix + resourceName);
-			if (file != null) return stream = file;
+			// tmr if (file != null) return stream = file;
+			if (file != null) return file;
 		} catch (Exception e) { }
 		return stream;
 	}
