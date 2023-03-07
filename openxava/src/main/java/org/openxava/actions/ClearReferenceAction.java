0dbc4ede7d7b55c33693616dffe0cbfc40ca5432
chungyentsai <39892208+chungyentsai@users.noreply.github.com>
2023-03-07 15:13:13 -0300

    setFocus

--- openxava/src/main/java/org/openxava/actions/ClearReferenceAction.java
+++ openxava/src/main/java/org/openxava/actions/ClearReferenceAction.java
@@ -10,8 +10,10 @@ public class ClearReferenceAction extends ReferenceBaseAction {
 	public void execute() throws Exception {
 		super.execute();
 		View referenceView = getReferenceSubview();
+		View view = getView();
 		Map<?, ?> values = referenceView.getKeyValues();
 		values.replaceAll( (k,v)->v=null);
+		view.setFocus(referenceView.getMemberName() + "." + referenceView.getSearchKeyName());
 		referenceView.setValuesNotifying(values);
 	}
 
