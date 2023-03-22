83d635e11b1bd6b92e785c5d799ad45a9c611615
javierpaniza <javierpaniza>
2023-03-22 21:34:11 +0100

    Azure code moved to XavaPro. Testing with two applications, working on needed authorization if already logged for the second app.

--- openxava/src/main/java/com/openxava/naviox/web/NaviOXFilter.java
+++ openxava/src/main/java/com/openxava/naviox/web/NaviOXFilter.java
@@ -68,6 +68,7 @@ public class NaviOXFilter implements Filter {
 						SignInHelper.signIn((HttpServletRequest) request, autologinUser); 
 					}					
 				}
+				// TMR ME QUEDÉ POR AQUÍ: ¿PONER AQUÍ LO DE PREGUNTAR POR LO DE AZURE?
 			}
 			session.setAttribute("xava.user", session.getAttribute("naviox.user")); // We use naviox.user instead of working only
 						// with xava.user in order to prevent some security hole using UrlParameters.setUser
