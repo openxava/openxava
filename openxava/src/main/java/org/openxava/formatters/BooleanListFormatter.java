1fc58608f73b33f42a9fdbee27c6724e1a7e2ade
Mahaswami Software <venkat@mahaswami.com>
2023-07-03 14:33:31 +0530

    Update BooleanListFormatter.java
    Code refactorization done in Boolean List Formatter.

--- openxava/src/main/java/org/openxava/formatters/BooleanListFormatter.java
+++ openxava/src/main/java/org/openxava/formatters/BooleanListFormatter.java
@@ -1,34 +1,33 @@
 package org.openxava.formatters;
 
-import javax.servlet.http.*;
+import org.openxava.model.meta.MetaProperty;
 
-import org.openxava.model.meta.*;
+import javax.servlet.http.HttpServletRequest;
 
-/**
- *  
- * @since 4.8
- * @author Javier Paniza
- */
 
 public class BooleanListFormatter implements IMetaPropertyFormatter {
-	
-	public String format(HttpServletRequest request, MetaProperty p, Object booleanValue) {
-		return toBoolean(booleanValue)?p.getLabel():"";
-	}
-	
-	public Object parse(HttpServletRequest request, MetaProperty p, String string) {
-		return null; // Not needed for list
-	}
-	
-	static boolean toBoolean(Object booleanValue) {
-		if (booleanValue instanceof Boolean) { 		
-			return ((Boolean) booleanValue).booleanValue();
-			
-		}
-		if (booleanValue instanceof Number) {
-			return ((Number) booleanValue).intValue() != 0;
-		}
-		return false;
-	}
+
+    public String format(HttpServletRequest request, MetaProperty p, Object booleanValue) {
+
+        if (booleanValue == null) {
+            return "";
+        }
+        return toBoolean(booleanValue)? "Yes" : "No";
+    }
+
+    public Object parse(HttpServletRequest request, MetaProperty p, String string) {
+        return null; // Not needed for list
+    }
+
+    static boolean toBoolean(Object booleanValue) {
+        if (booleanValue instanceof Boolean) {
+            return ((Boolean) booleanValue).booleanValue();
+
+        }
+        if (booleanValue instanceof Number) {
+            return ((Number) booleanValue).intValue() != 0;
+        }
+        return false;
+    }
 
 }
