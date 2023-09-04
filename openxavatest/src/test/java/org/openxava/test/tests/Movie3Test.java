cde338252bd9cc0efecad1452ffea73189337745
Tsai <Tsai@DESKTOP-SM459TR>
2023-09-04 12:05:30 -0300

    fix test

--- openxavatest/src/test/java/org/openxava/test/tests/Movie3Test.java
+++ openxavatest/src/test/java/org/openxava/test/tests/Movie3Test.java
@@ -15,17 +15,17 @@ public class Movie3Test extends MovieBaseTest {
 	public void testShowImageForFileAnnotationWhenPrint() throws Exception {
 		execute("Print.generatePdf");
 		assertContentTypeForPopup("application/pdf"); 
-		assertPopupPDFLine(3, "FORREST GUMP ROBERT ZEMECKIS ERIC ROTH Y WINSTON GROOM 7/6/1994 Forrest Gump Trailer");
-		assertPopupPDFLine(4, "GATTACA gattaca-trailer");
+		assertPopupPDFLine(3, "FORREST GUMP ROBERT ZEMECKIS ERIC ROTH Y WINSTON GROOM 7/6/1994 Forrest Gump Trailer.webm");
+		assertPopupPDFLine(4, "GATTACA gattaca-trailer.png");
 
 		execute("Print.generateExcel");
 		assertContentTypeForPopup("text/x-csv");		
 		StringTokenizer excel = new StringTokenizer(getPopupText(), "\n\r");
 		excel.nextToken();
 		String line1 = excel.nextToken();
-		assertEquals("\"FORREST GUMP\";\"ROBERT ZEMECKIS\";\"ERIC ROTH Y WINSTON GROOM\";\"7/6/1994\";\"Forrest Gump Trailer\"", line1);
+		assertEquals("\"FORREST GUMP\";\"ROBERT ZEMECKIS\";\"ERIC ROTH Y WINSTON GROOM\";\"7/6/1994\";\"Forrest Gump Trailer.webm\"", line1);
 		String line2 = excel.nextToken();
-		assertEquals("\"GATTACA\";\"\";\"\";;\"gattaca-trailer\"", line2);
+		assertEquals("\"GATTACA\";\"\";\"\";;\"gattaca-trailer.png\"", line2);
 	}
 		
 }
