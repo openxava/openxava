b29adc6d3b88ac6956e5cb4739a5a1095f9739ec
Tsai <Tsai@DESKTOP-SM459TR>
2023-09-08 09:46:15 -0300

    add assert

--- openxavatest/src/test/java/org/openxava/test/tests/ProductTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/ProductTest.java
@@ -125,6 +125,8 @@ public class ProductTest extends ModuleTestBase {
 		assertValueInList(2, "XAVA\n3\nUnit price: 0.00, Unit price in pesetas: 0"); 
 		
 		execute("List.viewDetail", "row=2");
+		assertValue("number", "3");
+		assertValue("description", "XAVA");
 		execute("Mode.list");
 		
 		assertFalse(getHtml().contains("There are no records"));
