5c495addd8d66144b7b96c92436a6c27f989fa8d
Tsai <Tsai@DESKTOP-SM459TR>
2023-09-12 12:36:49 -0300

    fix

--- openxavatest/src/test/java/org/openxava/test/tests/EventTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/EventTest.java
@@ -9,6 +9,7 @@ public class EventTest extends ModuleTestBase {
 	}
 	
 	public void testComboShowValueWhenHasCondition() throws Exception {
+		assertValueInList(0, 5, "DEBT"); 
 		execute("List.viewDetail", "row=0");
 		assertValue("debtAtDate.id", "4028e4ab8a8966d7018a896a5bf80000");
 	}
