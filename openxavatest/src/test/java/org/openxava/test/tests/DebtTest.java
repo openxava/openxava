ec55918410641d5cd7d08ba4ba25bf97ea9e3ab2
javierpaniza <javierpaniza>
2023-09-15 14:09:39 +0200

    Changelog sorted. DebtTest modified to work in different computers.

--- openxavatest/src/test/java/org/openxava/test/tests/DebtTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/DebtTest.java
@@ -14,46 +14,48 @@ public class DebtTest extends ModuleTestBase {
 	
 	public void testCheckBoxEditorWithNotBooleanValue() throws Exception {
 		execute("CRUD.new");
-		setValue("description", "AN IMPORTANT DEBT");
+		setValue("description", "IMPORTANT DEBT");
 		setValue("paid", "true");
 		setValue("important", "true"); // "true" instead of "IMPORTANT" because in tests the checkboxs only allow true/false to check and uncheck 	
 		execute("CRUD.save");				
-		setValue("description", "A NOT SO IMPORTANT DEBT");
+		setValue("description", "NOT SO IMPORTANT DEBT");
 		setValue("paid", "false");
 		setValue("important", "false"); 		
 		execute("CRUD.save");
 		
 		execute("Mode.list");
+		execute("List.orderBy", "property=description"); 
 		assertListRowCount(3);
+		assertValueInList(0, 0, "DEBT");
 		assertValueInList(0, 1, "Paid"); 
 		assertValueInList(0, 3, "NULL: TRUE");
-		assertValueInList(1, 0, "AN IMPORTANT DEBT");
+		assertValueInList(1, 0, "IMPORTANT DEBT");
 		assertValueInList(1, 1, "Paid"); 
 		assertValueInList(1, 2, "IMPORTANT");
 		assertValueInList(1, 3, "IMPORTANT: TRUE");
-		assertValueInList(2, 0, "A NOT SO IMPORTANT DEBT");
+		assertValueInList(2, 0, "NOT SO IMPORTANT DEBT");
 		assertValueInList(2, 1, ""); 
 		assertValueInList(2, 2, "");
-		assertValueInList(2, 3, "NULL: FALSE");
+		assertValueInList(2, 3, "NULL: FALSE");		
 		
 		execute("List.viewDetail", "row=1");
-		assertValue("description", "AN IMPORTANT DEBT");
+		assertValue("description", "IMPORTANT DEBT");
 		assertValue("paid", "true");
 		assertValue("important", "true"); 
 		assertValue("status", "IMPORTANT: TRUE");
 		execute("Navigation.next");
-		assertValue("description", "A NOT SO IMPORTANT DEBT");
+		assertValue("description", "NOT SO IMPORTANT DEBT");
 		assertValue("paid", "false");
 		assertValue("important", "false"); 
 		assertValue("status", "NULL: FALSE");
 		
 		execute("Navigation.previous");
-		assertValue("description", "AN IMPORTANT DEBT");
+		assertValue("description", "IMPORTANT DEBT");
 		setValue("paid", "false");
 		setValue("important", "false");
 		execute("CRUD.save");
 		execute("Mode.list");
-		assertValueInList(1, 0, "AN IMPORTANT DEBT");
+		assertValueInList(1, 0, "IMPORTANT DEBT");
 		assertValueInList(1, 1, ""); 
 		assertValueInList(1, 2, "");
 		assertValueInList(1, 3, "NULL: FALSE");
