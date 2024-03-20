0908e253dae2fd16a79239a057bd2e0c45837b7d
Tsai <tsai-chungyen@hotmail.com>
2024-03-20 10:14:27 -0300

    add test

--- openxavatest/src/test/java/org/openxava/test/tests/byfeature/MaskTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/byfeature/MaskTest.java
@@ -15,20 +15,18 @@ public class MaskTest extends WebDriverTestBase {
 		WebElement email = getDriver().findElement(By.id("ox_openxavatest_CustomerWithSection__email"));
 		assertTrue(email.getAttribute("data-inputmask") != null);
 		email.sendKeys("pedro.matias.g.f@gmail.com.ar");
-		String textInInput = email.getAttribute("value");
-		assertEquals("pedro.matias.g.f@gmail.com.ar", textInInput);
+		assertValue("email","pedro.matias.g.f@gmail.com.ar");
 		email.sendKeys("ab");
-		assertEquals("pedro.matias.g.f@gmail.com.ar", textInInput);
+		assertValue("email","pedro.matias.g.f@gmail.com.ar");
 		
 		WebElement emailList = getDriver().findElement(By.id("ox_openxavatest_CustomerWithSection__additionalEmails"));
 		assertTrue(emailList.getAttribute("data-inputmask") != null);
 		emailList.sendKeys("pedro@gmail.com, pedro2@gmail.com");
-		textInInput = emailList.getAttribute("value");
-		assertEquals("pedro@gmail.com, pedro2@gmail.com", textInInput);
+		assertValue("additionalEmails","pedro@gmail.com, pedro2@gmail.com");
 		emailList.sendKeys("@");
-		assertEquals("pedro@gmail.com, pedro2@gmail.com", textInInput);
+		assertValue("additionalEmails","pedro@gmail.com, pedro2@gmail.com");
 		emailList.sendKeys(".ar");
-		assertEquals("pedro@gmail.com, pedro2@gmail.com.ar", textInInput);
+		assertValue("additionalEmails","pedro@gmail.com, pedro2@gmail.com.ar");
 	}
 	
 }
