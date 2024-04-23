22223d8d0728492cca5d58266397c7d7d30fe320
Tsai <tsai-chungyen@hotmail.com>
2024-04-23 20:08:30 -0300

    add test

---
+++ openxavatest/src/test/java/org/openxava/test/tests/byfeature/CollectionTest.java
@@ -0,0 +1,26 @@
+package org.openxava.test.tests.byfeature;
+
+import org.openqa.selenium.*;
+
+public class CollectionTest extends WebDriverTestBase {
+	
+	public void testChangeSectionNotLoadCollection() throws Exception {
+		goModule("CustomerWithSection");
+		execute("CRUD.new");
+		setValue("number", "43");
+		execute("Sections.change", "activeSection=1");
+		assertCollectionRowCount("states", 0);
+		execute("CRUD.refresh");
+		assertCollectionRowCount("states", 1);
+		
+		goModule("Office");
+		execute("CRUD.new");
+		setValue("defaultCarrier.number","1");
+		WebElement label = getDriver().findElement(By.id("ox_openxavatest_Office__label_defaultCarrier___number"));
+		label.click();
+		wait(getDriver());
+		assertCollectionRowCount("defaultCarrier___fellowCarriers", (3+2));
+		assertCollectionRowCount("defaultCarrier___fellowCarriersCalculated", (3+1));
+	}
+
+}
