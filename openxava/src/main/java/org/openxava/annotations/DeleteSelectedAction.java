00c9adbd8da42a30fdf0973377bdb17734db083b
Tsai <tsai-chungyen@hotmail.com>
2024-07-12 13:00:27 -0300

    improve annotation doc

--- openxava/src/main/java/org/openxava/annotations/DeleteSelectedAction.java
+++ openxava/src/main/java/org/openxava/annotations/DeleteSelectedAction.java
@@ -3,7 +3,19 @@ package org.openxava.annotations;
 import java.lang.annotation.*;
 
 /**
+ * Allows to define your a custom action to delete the selected elements 
+ * from the collection and data base. <p>
+ *  
+ * Applies to @OneToMany/@ManyToMany collections. <p>
  * 
+ * This is the action executed when a
+ * user select some rows and then click in 'Delete selected' link.<br>
+ * Example:
+ * <pre>
+ * &nbsp;@OneToMany (mappedBy="delivery")
+ * &nbsp;@DeleteSelectedAction("DeliveryDetails.deleteSelected")
+ * &nbsp;private Collection<DeliveryDetail> details;	
+ * </pre>
  * @author Chungyen, Tsai
  *
  */
