9f3d1d6d27f9a1c318f44559312d5c369c72f05f
Tsai <tsai-chungyen@hotmail.com>
2024-07-12 10:06:15 -0300

    test another way

--- openxava/src/main/java/org/openxava/actions/DeleteSelectedInCollectionAction.java
+++ openxava/src/main/java/org/openxava/actions/DeleteSelectedInCollectionAction.java
@@ -3,36 +3,29 @@ package org.openxava.actions;
 import java.util.*;
 
 import org.openxava.model.*;
-import org.openxava.validators.*;
 
-public class DeleteSelectedInCollectionAction extends CollectionBaseAction {
-	
-	public void execute() throws Exception {
-		try{
-			Map [] selectedOnes = getSelectedKeys();
-			validateMinimum(selectedOnes.length);
-			if (selectedOnes.length > 0) {
-				for (Map values: selectedOnes) {
-					removeElement(values);
-				}				
-				commit(); // If we change this, we should run all test suite using READ COMMITED (with hsqldb 2 for example)				
-				addMessage("aggregate_removed", getCollectionElementView().getModelName());
-				getView().recalculateProperties();
-				getCollectionElementView().collectionDeselectAll();
-				getCollectionElementView().refreshCollections(); 
-			}
-		}
-		catch (ValidationException ex) {
-			addErrors(ex.getErrors());
-		}
-	}
+/**
+ * The default action to delete an element in a @OneToMany/@ManyToMany collection.
+ * 
+ * @author Chungyen Tsai
+ *
+ */
+public class DeleteSelectedInCollectionAction extends DeleteSelectedInCollectionBaseAction {
 	
 	/**
 	 * Is called for each selected row with the values that includes the key
 	 * values. <p>
 	 */
 	protected void removeElement(Map values) throws Exception {
-		MapFacade.remove(getCollectionElementView().getModelName(), values);
+		if (getCollectionElementView().getMetaCollection().isAggregate()) {
+			MapFacade.removeCollectionElement(getCollectionElementView().getParent().getModelName(), getCollectionElementView().getParent().getKeyValues(), getCollectionElementView().getMemberName(), values);
+		} else {
+			MapFacade.remove(getCollectionElementView().getModelName(), values);
+		}
+	}
+	
+	protected void addMessage() { 
+		addMessage("aggregate_removed", getCollectionElementView().getModelName());
 	}
 		
 }
--- openxava/src/main/java/org/openxava/actions/RemoveSelectedInCollectionAction.java
+++ openxava/src/main/java/org/openxava/actions/RemoveSelectedInCollectionAction.java
@@ -3,7 +3,6 @@ package org.openxava.actions;
 import java.util.*;
 
 import org.openxava.model.*;
-import org.openxava.validators.*;
 
 /**
  * The default action to remove an element in a @OneToMany/@ManyToMany collection.
@@ -15,27 +14,7 @@ import org.openxava.validators.*;
  * @author Javier Paniza 
  */
 
-public class RemoveSelectedInCollectionAction extends CollectionBaseAction {
-	
-	public void execute() throws Exception {
-		try{
-			Map [] selectedOnes = getSelectedKeys(); 
-			validateMinimum(selectedOnes.length); 
-			if (selectedOnes.length > 0) {
-				for (Map values: selectedOnes) {
-					removeElement(values);					
-				}				
-				commit(); // If we change this, we should run all test suite using READ COMMITED (with hsqldb 2 for example)				
-				addMessage();
-				getView().recalculateProperties();
-				getCollectionElementView().collectionDeselectAll();
-				getCollectionElementView().refreshCollections(); 
-			}
-		}
-		catch (ValidationException ex) {			
-			addErrors(ex.getErrors());
-		}
-	}
+public class RemoveSelectedInCollectionAction extends DeleteSelectedInCollectionBaseAction {
 	
 	/**
 	 * Is called for each selected row with the values that includes the key
