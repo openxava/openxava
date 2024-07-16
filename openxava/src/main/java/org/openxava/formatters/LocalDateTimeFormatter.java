3a0a51763a777c7bd567e2b7889b77c4e52be1e3
Tsai <tsai-chungyen@hotmail.com>
2024-07-16 14:08:04 -0300

    debugging

--- openxava/src/main/java/org/openxava/formatters/LocalDateTimeFormatter.java
+++ openxava/src/main/java/org/openxava/formatters/LocalDateTimeFormatter.java
@@ -3,6 +3,7 @@ package org.openxava.formatters;
 import java.text.*;
 import java.time.*;
 import java.time.format.*;
+import java.time.temporal.*;
 
 import javax.servlet.http.*;
 
@@ -14,12 +15,17 @@ public class LocalDateTimeFormatter extends DateTimeBaseFormatter implements IFo
 	
 	private static DateTimeFormatter [] extendedFormatters = { // Only for some locales like "es", "fr", "ca" and "pl"
 			DateTimeFormatter.ofPattern("d/M/yyyy HH:mm").withResolverStyle(ResolverStyle.SMART),
-	        DateTimeFormatter.ofPattern("d/M/yy HH:mm").withResolverStyle(ResolverStyle.SMART),
+			DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withResolverStyle(ResolverStyle.SMART),
+			DateTimeFormatter.ofPattern("dd/MM/yy HH:mm").withResolverStyle(ResolverStyle.SMART),
+			DateTimeFormatter.ofPattern("d/M/yy HH:mm").withResolverStyle(ResolverStyle.SMART),
 	        DateTimeFormatter.ofPattern("ddMMyy HH:mm").withResolverStyle(ResolverStyle.SMART),
 	        DateTimeFormatter.ofPattern("ddMMyyyy HH:mm").withResolverStyle(ResolverStyle.SMART),
+	        DateTimeFormatter.ofPattern("dd.MM.yy HH:mm").withResolverStyle(ResolverStyle.SMART),
 	        DateTimeFormatter.ofPattern("d.M.yy HH:mm").withResolverStyle(ResolverStyle.SMART),
 	        DateTimeFormatter.ofPattern("d.M.yyyy HH:mm").withResolverStyle(ResolverStyle.SMART),
-	        DateTimeFormatter.ofPattern("yyyy/M/d HH:mm").withResolverStyle(ResolverStyle.SMART)
+	        DateTimeFormatter.ofPattern("yyyy/M/d HH:mm").withResolverStyle(ResolverStyle.SMART),
+	        DateTimeFormatter.ofPattern("yyyy/M/d ah:mm").withResolverStyle(ResolverStyle.SMART),
+	        //DateTimeFormatter.ofPattern("yyyy/M/d ah:mm:ss").withResolverStyle(ResolverStyle.SMART),
 	};
 	
 	private static DateTimeFormatter dotFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"); // Only for some locales like "hr"
@@ -30,67 +36,51 @@ public class LocalDateTimeFormatter extends DateTimeBaseFormatter implements IFo
 	public String format(HttpServletRequest request, Object date) {
 		if (date == null) return "";
 		System.out.println("format " + date.toString());
-		if (date instanceof String || date instanceof Number) return date.toString();
-		System.out.println(1);
-		System.out.println(date instanceof java.time.LocalDateTime);
-		//if (((java.time.LocalDate)date).getYear() < 2) return "";
-		//if (date instanceof java.util.Date && Dates.getYear((java.util.Date)date) < 2) return ""; 
-		System.out.println(2);
-		if (isZhFormatAndJavaIs21orBetter()) return getDateTimeFormatter(false).format((LocalDateTime)date).replace("p. m.", "PM").replace("a. m.", "AM"); //use java 17 blank space \u00a0
-		/*
-		System.out.println(3);
-		Locale currentLocale = Locales.getCurrent();
-		System.out.println(currentLocale);
-		DateTimeFormatter formatter1 =DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).withLocale(currentLocale);
-		DateTimeFormatter formatter2 =DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(currentLocale);
-		DateTimeFormatter formatter3 =DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).withLocale(currentLocale);
-		DateTimeFormatter formatter4 =DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL).withLocale(currentLocale);
-		LocalDateTime now = LocalDateTime.now();
-		System.out.println("Fecha y hora formateada1: " + formatter1.format(now));
-		System.out.println("Fecha y hora formateada2: " + formatter2.format(now));
-		System.out.println("Fecha y hora formateada3: " + formatter3.format(now));
-		System.out.println("Fecha y hora formateada4: " + formatter4.format(now));
-		*/
+		if (date instanceof String || date instanceof Number) {
+			System.out.println("date instanceof String || date instanceof Number");
+			return date.toString();
+		}
+		//if (isZhFormat()) return getDateTimeFormatter(false).format((LocalDateTime)date).replace("-", "/");
+		//if (isZhFormatAndJavaIs21orBetter()) return getDateTimeFormatter(false).format((LocalDateTime)date).replace("p. m.", "PM").replace("a. m.", "AM"); //use java 17 blank space \u00a0
+		System.out.println(getDateTimeFormatter(false).format((LocalDateTime)date)); 
 		return getDateTimeFormatter(false).format((LocalDateTime)date); 
 	}
 
 	public Object parse(HttpServletRequest request, String string) throws ParseException {
 		if (Is.emptyString(string)) return null;
-		return null;
-		/*
 		System.out.println("parse " + string);
 		if (string.indexOf('-') >= 0 && !isDashFormat()) { // SimpleDateFormat does not work well with -
 			string = Strings.change(string, "-", "/");
 		}
 		System.out.println(1);
-		if (isZhFormatAndJavaIs21orBetter()) string = string.replace("PM", "p. m.").replace("AM", "a. m.");
+		//if (isZhFormatAndJavaIs21orBetter()) string = string.replace("PM", "p. m.").replace("AM", "a. m.");
 		System.out.println(2);
 		DateTimeFormatter [] dateTimeFormats = getDateTimeFormats();
+		
 		for (int i=0; i<dateTimeFormats.length; i++) {
 			try {
-				java.util.Date result = (java.util.Date) dateTimeFormats[i].parseObject(string);
-				return new java.sql.Timestamp( result.getTime() );
+				TemporalAccessor parsedDate = dateTimeFormats[i].parse(string);
+	            LocalDateTime result = LocalDateTime.from(parsedDate);
+				System.out.println(result);
+				return result;
 			}
-			catch (ParseException ex) {
+			catch (DateTimeParseException ex) {
 			} 
 		}
-		System.out.println(3);
-		java.util.Date result = (java.util.Date) new DateFormatter().parse(request, string);
-		return new java.sql.Timestamp( result.getTime() );*/
+		System.out.println("parse final");
+		LocalDateTime result = LocalDateTime.parse(string, getDateTimeFormatter(true));
+		return result; 
 	}
 
 	private DateTimeFormatter getDateTimeFormatter(boolean b) { 
 		if (isExtendedFormat()) return extendedFormatter;
 		if (isDotFormat()) return dotFormatter; 
-		if (isZhFormatAndJavaIs21orBetter()) return zhFormatter;
-		if (isZhFormatAndJavaLessThan9()) return zhFormatter;
-		System.out.println(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(Locales.getCurrent()));
-		System.out.println(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(Locales.getCurrent()).withResolverStyle(ResolverStyle.SMART));
+		if (isZhFormat()) return zhFormatter;
 		return DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(Locales.getCurrent());    
 	}
 	
 	private DateTimeFormatter[] getDateTimeFormats() {
-		if (isExtendedFormat() || isDotFormat() || isZhFormatAndJavaIs21orBetter()) return extendedFormatters;
+		if (isExtendedFormat() || isDotFormat() || isZhFormat()) return extendedFormatters;
 		return new DateTimeFormatter [] { getDateTimeFormatter(true) }; 
 	}
 	
