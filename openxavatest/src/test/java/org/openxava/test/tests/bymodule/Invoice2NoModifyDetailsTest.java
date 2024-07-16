32c6a76ac98f40ce73bcf2896c8affe89e909562
javierpaniza <javierpaniza>
2024-07-16 16:12:46 +0200

    Automated test for @LargeDisplay and default-format-label in editors finished

--- openxavatest/src/test/java/org/openxava/test/tests/bymodule/Invoice2NoModifyDetailsTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/bymodule/Invoice2NoModifyDetailsTest.java
@@ -31,12 +31,33 @@ public class Invoice2NoModifyDetailsTest extends ModuleTestBase {
 		assertNoModifyInCollection();
 	}
 
-	private void assertLargeDisplay() { 
+	private void assertLargeDisplay() throws Exception { 
 		assertSmallLabels(); // It also tests default-label-format for editor in editors.xml
-		HtmlElement amountsSumEditor = getHtmlPage().getHtmlElementById("ox_openxavatest_Invoice2NoModifyDetails__editor_amountsSum");
-		// TMR ME QUEDÉ POR AQUÍ
-		System.out.println("[Invoice2NoModifyDetailsTest.assertLargeDisplay] amountsSumEditor.asXml()=" + amountsSumEditor.asXml()); // tmr
-		System.out.println("[Invoice2NoModifyDetailsTest.assertLargeDisplay] amountsSumEditor.asNormalizedText()=" + amountsSumEditor.asNormalizedText());
+		assertDisplayValue("amountsSum", "\u20AC2,500.00"); // Euro and prefix
+		assertDisplayValue("vatPercentage", "16.0%"); // Percentage and suffix
+		assertDisplayValue("total", "2,650.00\u20AC"); // Automatic @Money suffix. Symbol and position depend on the server locale, not browser locale
+		// assertDisplayValue("total", "$2,650.00"); // Automatic @Money prefix. Starting openxavatest with -Duser.language=en -Duser.country=US
+		
+		assertEditorContains("vatPercentage", "<i class=\"mdi mdi-label-percent-outline\">"); // Icon
+		assertEditorNotContain("amountsSum", "<i class=\"mdi mdi"); // Icon
+		assertEditorContains("discount", "ox-large-display-negative"); // Negative style
+		assertEditorNotContain("amountsSum", "ox-large-display-negative"); // Negative style
+	}
+
+	private void assertEditorContains(String property, String expectedContainedContent) {
+		assertTrue(getEditor(property).asXml().contains(expectedContainedContent));
+	}
+	
+	private void assertEditorNotContain(String property, String expectedContainedContent) {
+		assertFalse(getEditor(property).asXml().contains(expectedContainedContent));
+	}	
+
+	private void assertDisplayValue(String property, String expectedValue) {
+		assertEquals(expectedValue, getEditor(property).asNormalizedText());
+	}
+
+	private HtmlElement getEditor(String property) {
+		return getHtmlPage().getHtmlElementById("ox_openxavatest_Invoice2NoModifyDetails__editor_" + property);
 	}
 
 	private void assertSmallLabels() {
