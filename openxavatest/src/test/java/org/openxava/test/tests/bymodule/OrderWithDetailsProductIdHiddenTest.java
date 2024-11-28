f8adeb2f5533772e0678789883a8ee6e5f016fc8
Tsai <tsai-chungyen@hotmail.com>
2024-11-28 12:16:00 -0300

    fix test

--- openxavatest/src/test/java/org/openxava/test/tests/bymodule/OrderWithDetailsProductIdHiddenTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/bymodule/OrderWithDetailsProductIdHiddenTest.java
@@ -22,7 +22,7 @@ public class OrderWithDetailsProductIdHiddenTest extends ModuleTestBase {
 		
 		changeModule("OrderWithDetailsProductIdHiddenPrimitive");
 		execute("List.viewDetail", "row=0");
-		assertValueInCollection("details", 0, "product.code", "1414");
+		assertValueInCollection("details2", 0, "product.code", "1414");
 		execute("CRUD.save");
 		execute("Mode.list");
 		execute("List.viewDetail", "row=0");
