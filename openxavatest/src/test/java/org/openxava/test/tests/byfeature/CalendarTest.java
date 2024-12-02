77bb9c14592be971fed1873cf3899864983d019c
Tsai <tsai-chungyen@hotmail.com>
2024-12-02 13:42:55 -0300

    add wait

--- openxavatest/src/test/java/org/openxava/test/tests/byfeature/CalendarTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/byfeature/CalendarTest.java
@@ -213,6 +213,7 @@ public class CalendarTest extends WebDriverTestBase {
 		moveToCalendarView(getDriver());
 		WebElement firstDayElement = getDriver().findElement(By.cssSelector(".fc-daygrid-day"));
 		firstDayElement.click();
+		wait(getDriver());
 		setValue("userName", "Pedro");
 		execute("CRUD.save");
 		execute("Mode.list");
