030221b2030e7f93fc118ef27bd278920d429dd2
Tsai <tsai-chungyen@hotmail.com>
2024-12-10 14:20:24 -0300

    fix, add test

--- openxavatest/src/test/java/org/openxava/test/tests/bymodule/OrderWithDetailsProductIdHiddenPrimitiveTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/bymodule/OrderWithDetailsProductIdHiddenPrimitiveTest.java
@@ -11,10 +11,14 @@ public class OrderWithDetailsProductIdHiddenPrimitiveTest extends ModuleTestBase
 	public void testElementCollectionCanSaveWithHiddenKeyPrimitive() throws Exception {
 		execute("List.viewDetail", "row=0");
 		assertValueInCollection("details2", 0, "product.code", "1414");
+		setValueInCollection("details2", 1, "product.code", "1515");
 		execute("CRUD.save");
 		execute("Mode.list");
 		execute("List.viewDetail", "row=0");
 		assertValueInCollection("details2", 0, "product.code", "1414");
+		assertValueInCollection("details2", 1, "product.code", "1515");
+		setValueInCollection("details2", 1, "product.code", "1515");
+		execute("CRUD.save");
 	}
 	
 }
