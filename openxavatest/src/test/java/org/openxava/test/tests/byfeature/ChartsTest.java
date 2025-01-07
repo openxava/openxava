f9c68739acb46a3290d4f7d2b65876c1fd6ab005
javierpaniza <javierpaniza>
2025-01-07 13:17:02 +0100

    Change end line character in ChartsTest

--- openxavatest/src/test/java/org/openxava/test/tests/byfeature/ChartsTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/byfeature/ChartsTest.java
@@ -3,7 +3,7 @@ package org.openxava.test.tests.byfeature;
 import java.util.*;
 import org.openqa.selenium.*;
 /**
- * To test charts related issues with Selenium.
+ * To test charts related issues with Seleniumx.
  * 
  * @author Javier Paniza
  */
