897f0458570b0969ca2a579b210d5d90b048918d
Tsai <tsai-chungyen@hotmail.com>
2025-01-08 01:31:53 -0300

    add if for javascript:void(0)

--- openxava/src/main/resources/META-INF/resources/xava/js/openxava.js
+++ openxava/src/main/resources/META-INF/resources/xava/js/openxava.js
@@ -57,7 +57,7 @@ openxava.refreshPage = function(result) {
 		window.location.reload();
 		return;
 	}		
-	if (result.forwardURL != null && result.forwardURL !== "javascript:void(0)") {
+	if (result.forwardURL != null) {
 		if (result.forwardInNewWindow) { 
 			if (openxava.newWindow != null) {
 				openxava.newWindow.location = result.forwardURL;
@@ -78,7 +78,7 @@ openxava.refreshPage = function(result) {
 			return; 			
 		}
 		else {
-			location.href=result.forwardURL;			
+			if (result.forwardURL !== "javascript:void(0)") location.href=result.forwardURL;			
 		}
 	}
 	else if (result.forwardURLs != null) { 
