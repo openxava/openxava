e413b34b124cd86e554354e2dbccf3850c676b3d
Tsai <tsai-chungyen@hotmail.com>
2025-01-09 11:45:34 -0300

    add test

---
+++ openxavatest/src/test/java/org/openxava/test/tests/bymodule/TrainingWithSectionsTest.java
@@ -0,0 +1,23 @@
+package org.openxava.test.tests.bymodule;
+
+import org.openxava.tests.*;
+
+
+
+/**
+ * 
+ * @author Chungyen Tsai
+ */
+
+public class TrainingWithSectionsTest extends ModuleTestBase {
+	
+	public TrainingWithSectionsTest(String testName) {
+		super(testName, "TrainingWithSections");		
+	}
+	
+	public void testOnChangeHideSection() throws Exception { 
+		assertNoErrors();
+	}
+
+		
+}
