b37a31539bebe8c31d4cafa6aea51baedf0d661d
javierpaniza <javierpaniza>
2025-01-23 15:44:01 +0100

    hotswap-agent.properties moved to OpenXava

--- openxava/src/main/java/org/openxava/util/OpenXavaPlugin.java
+++ openxava/src/main/java/org/openxava/util/OpenXavaPlugin.java
@@ -1,5 +1,7 @@
 package org.openxava.util;
 
+import java.lang.reflect.*;
+
 import org.hotswap.agent.annotation.*;
 
 /**
@@ -12,11 +14,25 @@ import org.hotswap.agent.annotation.*;
 public class OpenXavaPlugin {
 	
 	public static int cacheVersion = 0; // tmr En otro sitio, con un getter
+	
+	@Init
+    private static ClassLoader appClassLoader;
 
     @OnClassLoadEvent(classNameRegexp = ".*", events = LoadEvent.REDEFINE)
-    public static void onAnyReload() {
+    public static void onAnyReload() throws Exception {
+    	// Tmr ¿solo cuando se cambien entidades?
+        System.out.println("[OpenXavaPlugin.onAnyReload] " + cacheVersion); // tmr
+        System.out.println("[OpenXavaPlugin.onAnyReload] OpenXavaPlugin.class.getClassLoader()=" + OpenXavaPlugin.class.getClassLoader()); // tmr
+        System.out.println("[OpenXavaPlugin.onAnyReload] appClassLoader=" + appClassLoader); // tmr
+        
+        Method incrementCacheVersion = appClassLoader.loadClass(OpenXavaPlugin.class.getName())
+        	.getDeclaredMethod("incrementCacheVersion");
+        incrementCacheVersion.invoke(null);
+    }
+    
+    private static void incrementCacheVersion() {    	
     	cacheVersion++;
-        System.out.println("[OpenXavaPlugin.onAnyReload] " + cacheVersion); // tmr     
+    	System.out.println("[OpenXavaPlugin.incrementCacheVersion] cacheVersion=" + cacheVersion); // tmr
     }
     
 }
