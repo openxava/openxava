d315a6f7825705ebabc36f8052cada853254b2be
javierpaniza <javierpaniza@openxava.org>
2025-05-27 12:58:22 +0200

    Test to reproduce the bug created

--- openxavatest/src/test/java/org/openxava/test/tests/bymodule/OrderDocumentTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/bymodule/OrderDocumentTest.java
@@ -13,11 +13,24 @@ public class OrderDocumentTest extends ModuleTestBase {
 		super(testName, "OrderDocument");		
 	}
 			
+	// tmr Cambiar nombre del test
 	public void testCalculationPropertyInReferenceInsideHiddenSection() throws Exception {
 		getWebClient().getOptions().setCssEnabled(true);
+		
+		// Configure MessageConfirmHandler to detect confirmation dialogs
+		MessageConfirmHandler confirmHandler = new MessageConfirmHandler();
+		getWebClient().setConfirmHandler(confirmHandler);
+		
+		// Go to detail mode
 		execute("List.viewDetail", "row=0");
 		assertValue("year", "2024");
-		assertFalse(getHtmlPage().getElementById("xava_loading").isDisplayed());		
+		assertFalse(getHtmlPage().getElementById("xava_loading").isDisplayed());
+		
+		// Return to list mode without making any changes
+		execute("Mode.list");
+		
+		// Verify that no confirmation dialog has been shown
+		confirmHandler.assertNoMessage();
 	}
 				
 }
