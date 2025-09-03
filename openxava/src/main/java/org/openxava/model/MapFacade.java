6326d6d5a530419274490c51fc62b3ff0bfd07e0
javierpaniza <javierpaniza>
2025-09-03 20:19:10 +0200

    Refined and documented some code in MapFacade

--- openxava/src/main/java/org/openxava/model/MapFacade.java
+++ openxava/src/main/java/org/openxava/model/MapFacade.java
@@ -405,9 +405,11 @@ public class MapFacade {
 	}
 	
 	/**
-	 * tmr redoc incluyendo since, modificar doc de otros NotTracking
 	 * Obtain the specified values from entity/aggregate from a map with 
-	 * primary key values. <p>
+	 * primary key values, not tracking the query. <p>
+	 * 
+	 * Most methods of MapFacade track the changes/queries (using AccessTracker), 
+	 * but the ones with "NotTracking" suffix do not.<br>
 	 * 
 	 * The <code>memberNames</code> parameter is a map to use a treelike structure.<br>
 	 * The property names are in key part. If it's a simple property the value
@@ -421,6 +423,7 @@ public class MapFacade {
 	 * { "seller", { {"number", null}, {"name", null} } }
 	 * </pre>
 	 * 
+	 * @since 7.6
 	 * @param modelName  OpenXava model name. Not null.
 	 * @param keyValues  Key values of object to find. Not null. <i>By value</i> semantics.
 	 * @param memberNames Member names to obtain its values. Not null. <i>By value</i> semantics.  
@@ -513,72 +516,7 @@ public class MapFacade {
 			throw new SystemException(ex);
 		}			
 	}
-	
-	/**
-	 * tmr Doc
-	 * Obtain the specified values from entity/aggregate searching it by any property. <p>
-	 * 
-	 * The <code>memberNames</tt> parameter is a map to use a treelike structure.<br>
-	 * The property names are in key part. If it's a simple property the value
-	 * is null, otherwise it has a map with the same structure.<br>
-	 * For example, if we have a <code>Customer</tt> that references
-	 * to a <code>Seller</code>,
-	 * we can send a map with the next values:
-	 * <pre> 
-	 * { "number", null }
-	 * { "name", null }
-	 * { "seller", { {"number", null}, {"name", null} } }
-	 * </pre>
-	 * 
-	 * The <code>searchingValues</code> parameters are the values used to search. 
-	 * For example, if you can search by <i>name</i> and <i>surname</i> you can 
-	 * send to <code>searchingValues</code> a map with the next values: 	 
-	 * <pre> 
-	 * { "name", "JUAN" }
-	 * { "surname", "PEREZ" }
-	 * </pre>
-	 * In this case it returns the map with the value of the first "JUAN PEREZ" of database.<p>
-	 * 
-	 * If you use:
-	 * <pre>
-	 * { "name", "J" }
-	 * </pre> 
-	 * Then it returns the values for the first object of which <i>name</i> starts with 'J'.<p>
-	 * 
-	 * If you use:
-	 * <pre>
-	 * { "description", "%BIG" }
-	 * </pre>  
-	 * Then it returns the values for the first object of which <i>description</i> contains
-	 * "BIG".
-	 * 
-	 * @param modelName  OpenXava model name. Not null.
-	 * @param searchingValues  Values used for search the object. Not null. <i>By value</i> semantics.
-	 * @param memberNames Member names to obtain its values. Not null. <i>By value</i> semantics.  
-	 * @return Map with entity values. Not null.
-	 * @exception ObjectNotFoundException  If object with this key does not exist 
-	 * @exception FinderException  Logic problem on find.	
-	 * @exception XavaException  Any problem related to OpenXava. Rollback transaction.
-	 * @exception SystemException  System problem. Rollback transaction.
-	 */		
-	public static Map getValuesByAnyPropertyNotTracking(  // tmr Quitar
-		String modelName,
-		Map searchingValues,
-		Map memberNames)
-		throws FinderException, XavaException, SystemException 
-	{						
-		Assert.arg(modelName, searchingValues, memberNames);		
-		if (searchingValues.isEmpty()) {
-			throw new ObjectNotFoundException(XavaResources.getString("empty_key_object_not_found", modelName));						
-		}
-		try {
-			return getImpl().getValuesByAnyProperty(Users.getCurrentUserInfo(), modelName, searchingValues, memberNames);
-		}
-		catch (RemoteException ex) {
-			throw new SystemException(ex);
-		}			
-	}
-			
+				
 	/**
 	 * Obtain the values of the entity/aggregate from the own entity. <p> 
 	 * 
@@ -707,7 +645,8 @@ public class MapFacade {
 	/**
 	 * Set new values to a entity/aggregate that is found from its key values without tracking the changes. <p>
 	 * 
-	 * All methods of MapFacade track the changes (using AccessTracker), but this one.
+	 * Most methods of MapFacade track the changes/queries (using AccessTracker), 
+	 * but the ones with "NotTracking" suffix do not.
 	 * 
 	 * @since 5.9
 	 * @param modelName  OpenXava model name. Not null.
