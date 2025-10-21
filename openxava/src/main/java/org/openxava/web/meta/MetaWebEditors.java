fd75dcae21a487d1271c439c6cf6efa975c2b66e
javierpaniza <javierpaniza@openxava.org>
2025-10-21 11:53:45 +0200

    Bug fixed, test created. Suite remains

--- openxava/src/main/java/org/openxava/web/meta/MetaWebEditors.java
+++ openxava/src/main/java/org/openxava/web/meta/MetaWebEditors.java
@@ -373,6 +373,7 @@ public class MetaWebEditors {
 			if (editor.hasHasSet() && !"List".equals(editor.getName())) {
 				List<MetaProperty> mpList = new ArrayList<>(tab.getMetaModel().getMetaProperties());
 				for (MetaProperty mp : mpList) {
+					if (mp.isHidden()) continue; // tmr En changelog como bug y feature. Cambiar doc
 					if (editor.stereotypeMatches(mp) ||
 							editor.annotationMatches(editor, mp) ||
 							editor.typeMatches(mp)) {
