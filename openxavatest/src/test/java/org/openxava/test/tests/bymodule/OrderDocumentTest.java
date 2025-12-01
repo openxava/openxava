8f181ead82c5ceb05a33450bbb1383b86bc5181c
javierpaniza <4420198+javierpaniza@users.noreply.github.com>
2025-12-01 12:52:23 +0100

    Fix: Properties from references are excluded from the list when grouping

--- openxavatest/src/test/java/org/openxava/test/tests/bymodule/OrderDocumentTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/bymodule/OrderDocumentTest.java
@@ -13,8 +13,17 @@ public class OrderDocumentTest extends ModuleTestBase {
 		super(testName, "OrderDocument");		
 	}
 			
-	public void testCalculationPropertyInReferenceInsideHiddenSection() throws Exception {
+	public void testGroupByIncludesPropertiesFromReferences_CalculationPropertyInReferenceInsideHiddenSection() throws Exception {
 		getWebClient().getOptions().setCssEnabled(true);
+		
+		selectGroupBy("Group by year");
+		assertNoErrors();
+		assertListRowCount(1); 
+		assertValueInList(0, 0, "2024");
+		assertValueInList(0, 1, "26.62");
+		assertLabelInList(1, "Invoice total amount"); // No to test label, but to be sure that is the property from the reference
+		selectGroupBy("No grouping");
+		
 		execute("List.viewDetail", "row=0");
 		assertValue("year", "2024");
 		assertFalse(getHtmlPage().getElementById("xava_loading").isDisplayed());		
