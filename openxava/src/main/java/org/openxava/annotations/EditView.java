ddd3299b99a82859967fe4cf370d0fab829230fc
javierpaniza <javierpaniza@openxava.org>
2026-02-17 11:45:14 +0100

    Documentation for @EditView and @NewView

--- openxava/src/main/java/org/openxava/annotations/EditView.java
+++ openxava/src/main/java/org/openxava/annotations/EditView.java
@@ -1,20 +1,34 @@
 package org.openxava.annotations;
 
+
 import java.lang.annotation.*;
 
 /**
- * View of the referenced object used to modify it from a reference. <p>
+ * View of the referenced object used to modify it from a reference or collection. <p>
  * 
- * Applies to references. <p>
+ * Applies to references and collections. <p>
  * 
  * If you omit this annotation, then the default view of the referenced object 
  * is used when modifying the object from the reference. With this annotation 
  * you can indicate that it uses another view.<br>
- * Example:
+ * Example for a reference:
  * <pre>
  * &nbsp;@ManyToOne 
  * &nbsp;@EditView("SimpleEdition")
  * &nbsp;private Seller seller;
+ * </pre>
+ * 
+ * For collections, this annotation overrides the view specified by 
+ * {@link CollectionView} when editing an existing element. If you use 
+ * <code>@CollectionView("Simple")</code> it's equivalent to using both 
+ * <code>@NewView("Simple")</code> and <code>@EditView("Simple")</code>. 
+ * You can use <code>@EditView</code> to specify a different view just for editing.<br>
+ * Example for a collection:
+ * <pre>
+ * &nbsp;@OneToMany(mappedBy="invoice")
+ * &nbsp;@CollectionView("Simple")
+ * &nbsp;@EditView("SimpleEdition") // Overrides @CollectionView for editing
+ * &nbsp;private Collection&lt;InvoiceDetail&gt; details;
  * </pre>
  * 
  * @author Javier Paniza
--- openxava/src/main/java/org/openxava/annotations/NewView.java
+++ openxava/src/main/java/org/openxava/annotations/NewView.java
@@ -3,18 +3,31 @@ package org.openxava.annotations;
 import java.lang.annotation.*;
 
 /**
- * View of the referenced object used to create a new one from a reference. <p>
+ * View of the referenced object used to create a new one from a reference or collection. <p>
  * 
- * Applies to references. <p>
+ * Applies to references and collections. <p>
  * 
  * If you omit this annotation, then the default view of the referenced object 
  * is used when creating a new object from the reference. With this annotation 
  * you can indicate that it uses another view.<br>
- * Example:
+ * Example for a reference:
  * <pre>
  * &nbsp;@ManyToOne 
  * &nbsp;@NewView("SimpleCreation")
  * &nbsp;private Seller seller;
+ * </pre>
+ * 
+ * For collections, this annotation overrides the view specified by 
+ * {@link CollectionView} when creating a new element. If you use 
+ * <code>@CollectionView("Simple")</code> it's equivalent to using both 
+ * <code>@NewView("Simple")</code> and <code>@EditView("Simple")</code>. 
+ * You can use <code>@NewView</code> to specify a different view just for creation.<br>
+ * Example for a collection:
+ * <pre>
+ * &nbsp;@OneToMany(mappedBy="invoice")
+ * &nbsp;@CollectionView("Simple")
+ * &nbsp;@NewView("SimpleCreation") // Overrides @CollectionView for creation
+ * &nbsp;private Collection&lt;InvoiceDetail&gt; details;
  * </pre>
  * 
  * @author Javier Paniza
