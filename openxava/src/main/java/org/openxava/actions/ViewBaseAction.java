1af95185f4419465e6ef68c38a04ffeab0038829
javierpaniza <javierpaniza@openxava.org>
2026-02-19 13:32:17 +0100

    Fix: xava_view corrupted after calling on-change action

--- openxava/src/main/java/org/openxava/actions/ViewBaseAction.java
+++ openxava/src/main/java/org/openxava/actions/ViewBaseAction.java
@@ -161,7 +161,9 @@ abstract public class ViewBaseAction extends BaseAction {
 
 	public void setView(View view) {
 		this.view = view;
-		getContext().put(getRequest(), "xava_view", view); // For instant injection, so it can be use for actions called from executeAction()
+		if (!(this instanceof IOnChangePropertyAction)) { // In on change actions view is injected from View
+			getContext().put(getRequest(), "xava_view", view); // For instant injection, so it can be use for actions called from executeAction()
+		}
 	}
 			
 	protected String getModelName() {
