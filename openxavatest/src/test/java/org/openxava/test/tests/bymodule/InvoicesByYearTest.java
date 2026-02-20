a9e4af7d927356b7dfa2c502b1f61a18342b6fea
javierpaniza <javierpaniza@openxava.org>
2026-02-20 12:33:24 +0100

    Test created for filter in collections

--- openxavatest/src/test/java/org/openxava/test/tests/bymodule/InvoicesByYearTest.java
+++ openxavatest/src/test/java/org/openxava/test/tests/bymodule/InvoicesByYearTest.java
@@ -15,7 +15,7 @@ public class InvoicesByYearTest extends ModuleTestBase {
 		super(testName, "InvoicesByYear");		
 	}
 			
-	public void testCollectionCountInSectionNotRefreshedOnChangeCollection_collectionTotalsInTransientClassRecalculated() throws Exception {
+	public void testCollectionCountInSectionNotRefreshedOnChangeCollection_collectionTotalsInTransientClassRecalculated_filterInCollection() throws Exception {
 		assertCollectionRowCount("invoices", 0);
 		assertInvoicesCountInSection(0);
 		setValue("year", "2004");
@@ -26,6 +26,19 @@ public class InvoicesByYearTest extends ModuleTestBase {
 		execute("InvoicesByYear.setModel");
 		setValue("year", "2002");
 		assertTotalInCollection("invoices", "total", "2,900.00");
+		
+		// Test IFilter in collections (section 1)
+		execute("Sections.change", "activeSection=1");
+		assertCollectionRowCount("activeInvoices", 5);
+		for (int i = 0; i < 5; i++) {
+			assertValueInCollection("activeInvoices", i, "year", "2004");
+		}
+		assertCollectionRowCount("activeInvoicesPlusYear", 6);
+		for (int i = 0; i < 6; i++) {
+			assertTrue("Year should be 2004 or 2002", 
+				getValueInCollection("activeInvoicesPlusYear", i, "year").equals("2004") || 
+				getValueInCollection("activeInvoicesPlusYear", i, "year").equals("2002"));
+		}
 	}
 	
 	private void assertInvoicesCountInSection(int expectedCount) throws Exception {
