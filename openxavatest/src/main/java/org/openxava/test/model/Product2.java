8a61c921daf549873a768e5418921640f119bd0f
javierpaniza <javierpaniza@openxava.org>
2026-03-02 11:36:28 +0100

    Test created

--- openxavatest/src/main/java/org/openxava/test/model/Product2.java
+++ openxavatest/src/main/java/org/openxava/test/model/Product2.java
@@ -142,6 +142,7 @@ public class Product2 {
 	private String description;
 	
 	@Stereotype("IMAGES_GALLERY")
+	@Column(length = 32) // tmr Quitar
 	private String photos;
 	
 	@ManyToOne(fetch=FetchType.LAZY)
