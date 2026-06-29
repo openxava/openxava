fdf678a29cd864553e57ffda4b14fd28b3e15608
javierpaniza <javierpaniza@openxava.org>
2026-06-29 12:03:36 +0200

    TestServletTest passed

--- openxavatest/src/main/java/org/openxava/test/servlets/TestServlet.java
+++ openxavatest/src/main/java/org/openxava/test/servlets/TestServlet.java
@@ -2,14 +2,16 @@ package org.openxava.test.servlets;
 
 import java.io.IOException;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
- * 
+ *
  * @author Javier Paniza
  */
+@WebServlet("/test")
 public class TestServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
