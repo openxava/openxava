7947ec5694fca8509696f8118aef685b9920749e
javierpaniza <javierpaniza>
2026-08-12 21:23:57 +0200

    DBServer does a shutdown of db on JVM stop, so modified data is always stored in disk

--- openxava/src/main/java/org/openxava/util/DBServer.java
+++ openxava/src/main/java/org/openxava/util/DBServer.java
@@ -1,5 +1,6 @@
 package org.openxava.util;
 
+import java.sql.*;
 import java.util.logging.*;
 
 /**
@@ -9,12 +10,15 @@ import java.util.logging.*;
 
 public class DBServer {
 	
+	private static int port;
+	
 	public static void start(String dbName) throws Exception {
 		start(dbName, 1666); 
 	}
 	
 	/** @since 7.1 */
 	public static void start(String dbName, int port) throws Exception { 
+		DBServer.port = port;
 		new Thread(() -> {
 			org.hsqldb.Server hsqlServer = new org.hsqldb.Server();
 	        hsqlServer.setSilent(true);
@@ -24,6 +28,22 @@ public class DBServer {
 	        hsqlServer.start();     
 	        Logger.getLogger("").setLevel(Level.INFO);
 		}).start();
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> shutdown()));
+	}
+	
+	/**
+	 * Shuts down the HSQLDB server cleanly, persisting all in-memory data
+	 * to the <code>.script</code> file and clearing the <code>.log</code> file.
+	 * @since 8.0
+	 */
+	public static void shutdown() {
+		try (Connection con = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost:" + port, "sa", "");
+			Statement st = con.createStatement()) {
+			st.execute("SHUTDOWN");
+		}
+		catch (SQLException ex) {
+			// Server may already be down or not yet started
+		}
 	}
 	
 	public static void runManager() {
