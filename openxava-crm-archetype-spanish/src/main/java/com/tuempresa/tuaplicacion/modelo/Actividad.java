a705fcff42963f3a111bd4533a98515975721e88
javierpaniza <javierpaniza@openxava.org>
2026-09-17 15:34:28 +0200

    openxava-crm-archetype and openxava-crm-archetype-spanish adapted to v8 and tested

--- openxava-crm-archetype-spanish/src/main/java/com/tuempresa/tuaplicacion/modelo/Actividad.java
+++ openxava-crm-archetype-spanish/src/main/java/com/tuempresa/tuaplicacion/modelo/Actividad.java
@@ -2,7 +2,7 @@ package com.tuempresa.tuaplicacion.modelo;
 
 import java.time.*;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 import org.openxava.annotations.*;
 import org.openxava.calculators.*;
