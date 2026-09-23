405a743201a9b3ca1aca269d6103265bc63d2798
javierpaniza <javierpaniza>
2026-09-23 21:01:24 +0200

    openxava-doc upgraded to Jakarta EE 11 to work with Tomcat 11

--- openxava-doc/src/org/openxava/doc/filter/PlausibleAnalyticsFilter.java
+++ openxava-doc/src/org/openxava/doc/filter/PlausibleAnalyticsFilter.java
@@ -1,10 +1,10 @@
 package org.openxava.doc.filter;
 
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletResponseWrapper;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
